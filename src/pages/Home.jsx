23d888f4f51fd4fa26b354ab744c02e2251fe78b
KriteshTimsina <kriteshtimsina55@gmail.com>
2023-03-18 20:33:59 +0545

    added copy to clipboard functionality in random color generator

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
       <Link to={"/bill-calculator"}   className='cursor-pointer bg-cyan-700  text-center hover:text-slate-200 hover:bg-cyan-900 hover:font-medium hover:pt-1 transition-all h-20 w-20 px-1 py-2'>
         Bill Split App
       </Link> 
-      <Link to="/bmi-ca" className='cursor-pointer bg-purple-700  text-center hover:text-slate-200 hover:bg-purple-900 hover:font-medium hover:pt-1 transition-all h-20 w-20 px-1 py-2'>
-        BMI Calculator
-      </Link>
       <Link to="/random" className='cursor-pointer bg-blue-700  text-center hover:text-slate-200 hover:bg-blue-900 hover:font-medium hover:pt-1 transition-all h-20 w-20 px-1 py-2'>
         Random color generator
+      </Link>
+      <Link to="/bmi-ca" className='cursor-pointer bg-purple-700  text-center hover:text-slate-200 hover:bg-purple-900 hover:font-medium hover:pt-1 transition-all h-20 w-20 px-1 py-2'>
+        BMI Calculator
       </Link>
       
     </div>
--- src/pages/randomColorGenerator/Index.jsx
+++ src/pages/randomColorGenerator/Index.jsx
@@ -1,52 +1,58 @@
-import React,{useEffect} from 'react'
-import {AiOutlineCopy} from 'react-icons/ai';
+import React from "react";
+import { AiOutlineCopy } from "react-icons/ai";
+import { toast,ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 const Index = () => {
-  let divElement = [];
-  let div = [];
+  //initializing empty divElement array to loop 
+  const divElement = [];
+
+  //this function generates random hex color for-eg: #a0i990
+  let generateRandomColor = () => {
+    const chars = "0123456789abcdef";
+    let colorCodeLength = 6;
+    let colorCode = "#";
+    for (let i = 0; i < colorCodeLength; i++) {
+      const randomNumber = Math.floor(Math.random() * 16);
+      colorCode += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return colorCode;
+  };
+  //copy text on button click
+  const handleCopyText = async(randomColor) =>{
+    try{
+          await navigator.clipboard.writeText(randomColor);
+          toast(`Successfully copied "${randomColor}"`, { autoClose: 2000 });
+        }catch(e){
+          toast.error("Something went wrong!", { autoClose: 2000 });
+        }
+  }
+
+  //this block loops for 30 times and generate 30 div element
+  for (let i = 0; i <50; i++) {
+    let randomColor = generateRandomColor()
+    divElement.push(
+      <div title="click to copy" onClick={()=>handleCopyText(randomColor)} key={i} style={{ backgroundColor: randomColor, boxShadow:"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px" }} className="cursor-pointer relative p-5 drop-shadow-lg rounded-sm mx-1">
+        {randomColor}
+        <AiOutlineCopy onClick={()=>handleCopyText(randomColor)} className=" absolute top-1 right-1" />
+      </div>
+    );
+  }
+   let rand = generateRandomColor();
  
-  const generateRandomColor = ()=>{
-  const chars = "0123456789abcdef";
-  let colorCodeLength = 6;
-  let colorCode = "" ;
-
-  for (let i=0;i<colorCodeLength;i++){
-    const randomNumber = Math.floor(Math.random()*16);
-    colorCode+= chars.substring(randomNumber,randomNumber+1);
-  }
-  return colorCode 
-}
-const randomNumber = "#"+  generateRandomColor();
-divElement.push(randomNumber);
-async function handleCopyText(){
-  try{
-    await navigator.clipboard.writeText(randomNumber);
-    alert("Done")
-  }catch(e){
-    console.log(e);
-  }
-}
-
-
-// for(let i=0;i<2;i++){
-//   div.push(<div className='p-5' style={{backgroundColor:randomNumber}}>{randomNumber}</div>);
-// }
-
   return (
-    <div className='flex flex-col justify-start items-center h-screen gap-5 flex-wrap my-7'>
-    {
-    divElement.map((i,index)=>{
-        return(
-          <div key={index} className='relative'>
-          <div style={{backgroundColor:randomNumber}} className="p-5 px-7">{randomNumber}</div>
-          <button onClick={handleCopyText}><AiOutlineCopy className='absolute top-1 right-1 ' /></button>
-          </div>
-        )
-      })
-}
-<button className="py-2 px-5 bg-red-400 rounded-md">GenerateRandomColor</button>
-
+    <div className="my-5 flex flex-col items-center gap-5 bg-slate-900">
+    <h1 className="text-4xl text-center my-5 font-bold" style={{color:rand}}>Random Color Generator</h1>
+    <div className="flex  justify-center  items-center gap-5 flex-wrap">
+      {divElement}
     </div>
-  )
-}
+    {/* reloads page on button click */}
+    <button onClick={()=>setTimeout(()=>window.location.reload(),500)} style={{backgroundColor:rand}} className="p-2 rounded-sm bg-black ">Generate More</button>
+    <ToastContainer />
+    </div>
+  );
+};
 
-export default Index
+export default Index;
